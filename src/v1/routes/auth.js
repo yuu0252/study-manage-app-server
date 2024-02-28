f0ddf8fd57a26147d9afac545cb4d3a810ee1328
yuu0252 <yutanmaron@gmail.com>
2024-02-28 23:14:25 +0900

    edit auth

--- src/v1/routes/auth.js
+++ src/v1/routes/auth.js
@@ -4,3 +4,11 @@ require("dotenv").config();
 
 const User = require("../models/user");
 const validation = require("../middlewares/validation");
+const userController = require("../controllers/user");
+const tokenHandler = require("../middlewares/tokenHandler");
+
+router
+  .post
+  // '/register', body('username').isLength({min: 2}).withMessage('ユーザ名は2文字以上である必要があります'),
+  // body('password').isLength({min: 4}).withMessage('パスワードは4文字以上である必要があります');
+  ();
