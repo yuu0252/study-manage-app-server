e2086fe93f34ba2edc2fe45f32fff4536f0ef923
yuu0252 <yutanmaron@gmail.com>
2024-03-17 19:40:26 +0900

    create chat function

--- src/v1/controllers/openAi.js
+++ src/v1/controllers/openAi.js
@@ -1,9 +1,23 @@
 require("dotenv").config();
+const OpenAI = require("openai");
 
-exports.getData = async (req, res) => {
-  const { prompt } = req.body;
+const SECRET_KEY = process.env.OPENAI_SECRET_KEY;
+const openai = new OpenAI({ apiKey: SECRET_KEY });
 
-  const SECRET_KEY = process.env.OPENAI_SECRET_KEY;
+exports.chat = async (req, res) => {
+  try {
+    const { prompt } = req.body;
 
-  res.status(200).json(SECRET_KEY);
+    if (!prompt || prompt == "")
+      return res.status(400).json("プロンプトが不正です");
+
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: prompt }],
+      model: "gpt-3.5-turbo",
+    });
+
+    res.status(200).json(completion.choices[0].message.content);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
--- src/v1/routes/openAi.js
+++ src/v1/routes/openAi.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
 const openAiController = require("../controllers/openAi");
+const { verifyToken } = require("../middlewares/tokenHandler");
 
-router.post("/chat", openAiController.chat);
+router.post("/chat", verifyToken, openAiController.chat);
+
+module.exports = router;
